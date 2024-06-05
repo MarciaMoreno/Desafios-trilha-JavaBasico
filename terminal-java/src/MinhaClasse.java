7d2fac8ef9fababea2230dfe6c114f263a8c7a6c
MarciaMoreno <mapat.soad@gmail.com>
2024-06-05 20:23:26 -0300

    Diretorios argumentado via terminal

--- terminal-java/src/MinhaClasse.java
+++ terminal-java/src/MinhaClasse.java
@@ -1,6 +1,6 @@
 public class MinhaClasse {
     
     public static void main(String[] args) {
-        System.out.println("Oi, estou executando pelo terminal prompt");
+        System.out.println("Oi turma, estou executando pelo terminal prompt");
     }
 }
