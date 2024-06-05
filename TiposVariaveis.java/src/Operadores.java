10f5298ac1e5118203105f52e7afcc74f72b15d1
MarciaMoreno <mapat.soad@gmail.com>
2024-06-05 19:39:44 -0300

    Codificando um sistema de SmartTV

---
+++ TiposVariaveis.java/src/Operadores.java
@@ -0,0 +1,10 @@
+public class Operadores {
+    
+    public static void main(String[] args) {
+        boolean variavel = true;
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+    }
+}
---
+++ TiposVariaveis.java/src/Operadores2.java
@@ -0,0 +1,11 @@
+public class Operadores2 {
+    public static void main(String[] args) {
+        int a, b;
+        a = 5; 
+        b = 6;
+        String resultado = a==b ?"verdadeiro" : "falso";
+
+        System.out.println(resultado);
+    }
+    
+}
