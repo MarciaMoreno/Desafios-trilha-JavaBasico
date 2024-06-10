5db90a3691a952a6d9ae5da36f5e0dade439e60b
MarciaMoreno <mapat.soad@gmail.com>
2024-06-10 00:16:26 -0300

    Inserindo novas classes para estrutura de repetições

---
+++ Controles-de-Fluxos/src/ExemploDoWhile.java
@@ -0,0 +1,12 @@
+// ExemploDoWhile.java
+
+import java.util.Random;
+public class ExemploDoWhile {
+    public static void main(String[] args) {
+        boolean condicao = false;
+
+        while(condicao){
+            System.out.println("executou ... ");
+        }
+    }
+}
