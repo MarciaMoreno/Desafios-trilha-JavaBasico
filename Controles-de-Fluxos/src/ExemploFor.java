1ba0ef00581a2847a007bdc8f1ba4c509a13c3d9
MarciaMoreno <mapat.soad@gmail.com>
2024-06-09 23:17:19 -0300

    Exemplo for

--- Controles-de-Fluxos/src/ExemploFor.java
+++ Controles-de-Fluxos/src/ExemploFor.java
@@ -3,5 +3,6 @@ public class ExemploFor {
         for( int carneirinhos = 1; carneirinhos <=20; carneirinhos ++){
             System.out.println("Contando carneirinhos" + carneirinhos);
         }
+        System.out.println("Joãozinho dormiu!");
     }
 }
